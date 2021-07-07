9a9180031a5624aa824078703dc58c1841ec6981
Youssef1313 <youssefvictor00@gmail.com>
2021-07-07 14:17:42 +0200

    Fix

--- actions/docs-verifier/src/RedirectionVerifier/DocfxConfigurationReader.cs
+++ actions/docs-verifier/src/RedirectionVerifier/DocfxConfigurationReader.cs
@@ -23,7 +23,20 @@ namespace RedirectionVerifier
         /// <exception cref="InvalidOperationException">Failed to read <c>docfx.json</c>.</exception>
         public ImmutableArray<Matcher> GetMatchers()
         {
+            // Assume _docfxConfigurationFileName is "/github/workspace/path/to/docfx.json"
+            // Then we want to get only "path/to"
             string docfxPathRelativeToWorkspace = Path.GetRelativePath(relativeTo: Directory.GetCurrentDirectory(), Path.GetDirectoryName(_docfxConfigurationFileName)!);
+            if (docfxPathRelativeToWorkspace.StartsWith("./", StringComparison.Ordinal))
+            {
+                docfxPathRelativeToWorkspace = docfxPathRelativeToWorkspace[2..];
+            }
+            else
+            {
+                throw new InvalidOperationException($@"Expected docfx relative directory to start in ./, found {docfxPathRelativeToWorkspace}
+Docfx path: {_docfxConfigurationFileName}
+relativeTo: {Directory.GetCurrentDirectory()}");
+            }
+
             // If there are cached configuration values for "docfx", use 'em.
             if (cachedDocfxConfiguration is not null)
             {
