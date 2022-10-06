ee51d1530571968d82f9b3a41592cb1fbd2f3046
David Pine <david.pine@microsoft.com>
2022-10-05 22:10:32 -0500

    Port over, and update dependabotdotnet

---
+++ actions/dependabotdotnet/src/dependabotdotnet/GlobalUsings.cs
@@ -0,0 +1,7 @@
+﻿global using System.Diagnostics.CodeAnalysis;
+global using System.Net.Http.Json;
+global using System.Text;
+global using System.Text.RegularExpressions;
+global using Microsoft.Extensions.FileSystemGlobbing;
+global using Microsoft.Extensions.FileSystemGlobbing.Abstractions;
+global using static System.Console;
