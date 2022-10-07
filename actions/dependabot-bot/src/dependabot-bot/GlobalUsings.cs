d75cdf34d9d6f424dc4344003502e0ca43744798
David Pine <david.pine@microsoft.com>
2022-10-07 12:09:37 -0500

    Port dependabot dotnet (#55)
    * Port over, and update dependabotdotnet
    
    * A few minor tweaks
    
    * A few more fixes and tweaks
    
    * Use globals
    
    * Added bits from an open PR https://github.com/dotnet/core/pull/7845
    
    * Enforce update node limit
    
    * Fix link in README
    
    * Create action YML, and rename a few bits.
    
    * Usings...
    
    * A few tweaks
    
    * Update to use regex instead of manual string parsing.
    
    * Update actions/dependabot-bot/README.md
    
    Co-authored-by: Nick Schonning <nschonni@gmail.com>
    
    Co-authored-by: Nick Schonning <nschonni@gmail.com>

---
+++ actions/dependabot-bot/src/dependabot-bot/GlobalUsings.cs
@@ -0,0 +1,8 @@
+﻿global using Microsoft.Extensions.FileSystemGlobbing;
+global using Microsoft.Extensions.FileSystemGlobbing.Abstractions;
+global using System.Diagnostics.CodeAnalysis;
+global using System.Reflection;
+global using System.Text;
+global using System.Text.Json;
+global using System.Text.RegularExpressions;
+global using static System.Console;
