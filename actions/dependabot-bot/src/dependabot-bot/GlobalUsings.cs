189d21bbe4afff92d06dd91777d64ba2573cecd1
David Pine <david.pine@microsoft.com>
2022-10-12 09:32:29 -0500

    Remove embedded resource, use UTTP instead

--- actions/dependabot-bot/src/dependabot-bot/GlobalUsings.cs
+++ actions/dependabot-bot/src/dependabot-bot/GlobalUsings.cs
@@ -1,6 +1,7 @@
 ﻿global using Microsoft.Extensions.FileSystemGlobbing;
 global using Microsoft.Extensions.FileSystemGlobbing.Abstractions;
 global using System.Diagnostics.CodeAnalysis;
+global using System.Net.Http.Json;
 global using System.Reflection;
 global using System.Text;
 global using System.Text.Json;
