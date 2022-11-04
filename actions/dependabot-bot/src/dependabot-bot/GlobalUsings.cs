cfee17565ee008587e707bbcbc0ac78ae9ae156c
David Pine <david.pine@microsoft.com>
2022-11-04 07:52:33 -0500

    Refactored the dependabot-bot

--- actions/dependabot-bot/src/dependabot-bot/GlobalUsings.cs
+++ actions/dependabot-bot/src/dependabot-bot/GlobalUsings.cs
@@ -4,6 +4,6 @@ global using System.Diagnostics.CodeAnalysis;
 global using System.Net.Http.Json;
 global using System.Reflection;
 global using System.Text;
-global using System.Text.Json;
+global using Dependabot.Bot.Extensions;
 global using System.Text.RegularExpressions;
 global using static System.Console;
---
+++ actions/dependabot-bot/src/dependabot-bot/Program.Regex.cs
@@ -0,0 +1,44 @@
+﻿static partial class Program
+{
+    [GeneratedRegex("PackageReference.*Version=\"[0-9]")]
+    private static partial Regex PackageReferenceVersionRegex();
+
+    [GeneratedRegex("TargetFramework(.*)>(?<tfm>.+?)</")]
+    private static partial Regex TargetFrameworkRegex();
+
+    [GeneratedRegex("<PackageReference(?:.+?)Include=\"\"(?<nuget>.+?)\"\"")]
+    private static partial Regex PackageReferenceIncludeRegex();
+
+    static bool TryGetTargetFramework(
+    string content,
+    [NotNullWhen(true)] out string? targetFramework) =>
+    TryGetRegexGroupValue(
+        TargetFrameworkRegex(),
+        content, "tfm", out targetFramework);
+
+    static bool TryGetPackageName(
+        string content,
+        [NotNullWhen(true)] out string? packageName) =>
+        TryGetRegexGroupValue(
+            PackageReferenceIncludeRegex(),
+            content, "nuget", out packageName);
+
+    static bool TryGetRegexGroupValue(
+        Regex regex, 
+        string content, 
+        string groupKey, 
+        [NotNullWhen(true)] out string? groupValue)
+    {
+        var match = regex.Match(content);
+        if (match is { Success: true } and { Groups.Count: > 0 })
+        {
+            groupValue = match.Groups[groupKey].Value;
+            return true;
+        }
+        else
+        {
+            groupValue = null;
+            return false;
+        }
+    }
+}
