62812a405152ab5f6a71ad95ca365dcf2b5c46ae
David Pine <david.pine@microsoft.com>
2023-01-19 09:58:25 -0600

    Update TypeScript code, package refs, and tests

--- actions/status-checker/__tests__/main.test.ts
+++ actions/status-checker/__tests__/main.test.ts
@@ -1,28 +1,30 @@
-import {wait} from '../src/wait'
-import * as process from 'process'
-import * as cp from 'child_process'
-import * as path from 'path'
+import { wait } from "../src/wait";
+import { env, execPath } from "process";
+import { execFile, ExecFileOptions } from "child_process";
+import { join } from "path";
 
-test('throws invalid number', async () => {
-  const input = parseInt('foo', 10)
-  await expect(wait(input)).rejects.toThrow('milliseconds not a number')
-})
-
-test('wait 500 ms', async () => {
-  const start = new Date()
-  await wait(500)
-  const end = new Date()
-  var delta = Math.abs(end.getTime() - start.getTime())
-  expect(delta).toBeGreaterThan(450)
-})
-
-// shows how the runner will run a javascript action with env / stdout protocol
-test('test runs', () => {
-  process.env['INPUT_MILLISECONDS'] = '500'
-  const np = process.execPath
-  const ip = path.join(__dirname, '..', 'lib', 'main.js')
-  const options: cp.ExecFileSyncOptions = {
-    env: process.env
-  }
-  console.log(cp.execFileSync(np, [ip], options).toString())
-})
+describe("status-checker", () => {
+  it("throws invalid number", async () => {
+    const input = parseInt("foo", 10);
+    await expect(wait(input)).rejects.toThrow("milliseconds not a number");
+  });
+  
+  it("waits 500 ms", async () => {
+    const start = new Date();
+    await wait(500);
+    const end = new Date();
+    var delta = Math.abs(end.getTime() - start.getTime());
+    expect(delta).toBeGreaterThan(450);
+  });
+  
+  // Shows how the runner will run a javascript action with env / stdout protocol
+  it("runs test", async () => {
+    env["INPUT_MILLISECONDS"] = "500";
+    const np = execPath;
+    const ip = join(__dirname, "..", "lib", "main.js");
+    const options: ExecFileOptions = {
+      env: env,
+    };
+    console.log(await execFile(np, [ip], options).toString());
+  });
+});
