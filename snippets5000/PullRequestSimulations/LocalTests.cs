3139a8f11f5b9962a75e679f9d32db9129898621
Andy De George (adegeo) <adegeo@microsoft.com>
2023-04-03 12:08:58 -0700

    Add testing framework for file scan logic

---
+++ snippets5000/PullRequestSimulations/LocalTests.cs
@@ -0,0 +1,64 @@
+using LocateProjects;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.VisualStudio.TestTools.UnitTesting.Logging;
+using System.Reflection;
+using System.Text;
+
+namespace PullRequestSimulations;
+
+[TestClass]
+public partial class LocalTests
+{
+    PullRequest[] Requests;
+    string CurrentFolder;
+
+    public TestContext TestContext { get; set; }
+
+    [TestInitialize]
+    public void Init()
+    {
+        CurrentFolder = Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location)!;
+        Directory.SetCurrentDirectory(CurrentFolder);
+        Requests = PullRequest.LoadTests("data.json");
+        Logger.LogMessage($"Current Folder is: {CurrentFolder}");
+    }
+
+    public void RunTest(string name)
+    {
+        Logger.LogMessage("Running " + name);
+        PullRequest testItem = Requests.Where(p => p.Name.Equals(name, StringComparison.InvariantCultureIgnoreCase)).First();
+
+        if (testItem.ExpectedResults != null)
+        {
+            Logger.LogMessage($"Expected results:");
+
+            foreach (var result in testItem.ExpectedResults)
+                Logger.LogMessage($"  {result.ResultCode}|{result.DiscoveredProject}");
+        }
+        else
+            Logger.LogMessage($"No results expected");
+
+
+        foreach (var item in testItem.Items)
+        {
+            Logger.LogMessage($"\nProcessing item: {item.ItemType}\n  {item.Path}");
+
+            if (item.ItemType != ChangeItemType.Delete)
+                Assert.IsTrue(File.Exists(Path.GetFullPath(Path.Combine(CurrentFolder, item.Path))));
+
+            Logic.Test(CurrentFolder, item.Path, System.Array.Empty<string>(), out Logic.Result? resultItem);
+
+            if (!resultItem.HasValue)
+            {
+                Logger.LogMessage($"  No result, is expected? {testItem.ExpectedResults == null}");
+                Assert.IsNull(testItem.ExpectedResults);
+            }
+            else
+            {
+                Logger.LogMessage($"  {resultItem.Value.Code}|{resultItem.Value.DiscoveredFile}");
+                Assert.IsNotNull(testItem.ExpectedResults);
+                Assert.IsTrue(testItem.ExpectedResults.Where(r => r.ResultCode == resultItem.Value.Code && resultItem.Value.DiscoveredFile.EndsWith(r.DiscoveredProject, StringComparison.InvariantCultureIgnoreCase)).Count() == 1);
+            }
+        }
+    }
+}
---
+++ snippets5000/PullRequestSimulations/PullRequest.cs
@@ -0,0 +1,54 @@
+﻿using System.Text.Json.Serialization;
+using System.Text.Json;
+
+namespace PullRequestSimulations;
+
+public class PullRequest
+{
+    public string Name { get; set; }
+
+    public ChangeItem[] Items { get; set; }
+
+    public DiscoveryResult[] ExpectedResults { get; set; }
+
+    public PullRequest() { }
+
+    public static PullRequest[] LoadTests(string file)
+    {
+        JsonSerializerOptions options = new JsonSerializerOptions() { WriteIndented = true, Converters = { new JsonStringEnumConverter() }, ReadCommentHandling = JsonCommentHandling.Skip };
+
+        return JsonSerializer.Deserialize<PullRequest[]>(System.IO.File.ReadAllText(file), options)!;
+    }
+}
+
+public struct ChangeItem
+{
+    public ChangeItemType ItemType { get; init; }
+
+    public string Path { get; init; }
+
+    public ChangeItem(ChangeItemType itemType, string path)
+    {
+        ItemType = itemType;
+        Path = path;
+    }
+}
+
+public struct DiscoveryResult
+{
+    public int ResultCode { get; init; }
+    public string DiscoveredProject { get; init; }
+
+    public DiscoveryResult(int resultCode, string discoveredProject)
+    {
+        ResultCode = resultCode;
+        DiscoveredProject = discoveredProject;
+    }
+}
+
+public enum ChangeItemType
+{
+    Create,
+    Edit,
+    Delete
+}
