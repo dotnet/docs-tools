abdb0d32ad40eaa9e172a873f66ba2d37487d8b1
David Pine <david.pine@microsoft.com>
2024-01-22 09:25:10 -0600

    Expand our dependabot-bot to prioritize solutions, and then apply groups (#291)

--- actions/dependabot-bot/src/dependabot-bot/GlobalUsings.cs
+++ actions/dependabot-bot/src/dependabot-bot/GlobalUsings.cs
@@ -1,9 +1,8 @@
-﻿global using Microsoft.Extensions.FileSystemGlobbing;
-global using Microsoft.Extensions.FileSystemGlobbing.Abstractions;
+﻿global using Dependabot.Bot.Extensions;
+
 global using System.Diagnostics.CodeAnalysis;
 global using System.Net.Http.Json;
-global using System.Reflection;
 global using System.Text;
-global using Dependabot.Bot.Extensions;
 global using System.Text.RegularExpressions;
+
 global using static System.Console;
---
+++ actions/dependabot-bot/src/dependabot-bot/Program.Services.cs
@@ -0,0 +1,14 @@
+﻿using Pathological.ProjectSystem.Services;
+using Microsoft.Extensions.DependencyInjection;
+
+static partial class Program
+{
+    static IDiscoveryService AddAndGetDiscoveryService()
+    {
+        var serviceProvider = new ServiceCollection()
+            .AddDotNetProjectSystem()
+            .BuildServiceProvider();
+
+        return serviceProvider.GetRequiredService<IDiscoveryService>();
+    }
+}
