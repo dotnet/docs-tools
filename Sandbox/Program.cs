b39e668de6b9121d3fc221c9831c8cca57934c67
David Pine <david.pine@microsoft.com>
2024-04-22 13:48:44 -0500

    Add a test sandbox, and expose a means for getting a hydrated `OspoClient` via a factory that does `az login` and `az account get-access-token`.

---
+++ Sandbox/Program.cs
@@ -0,0 +1,10 @@
+﻿using DotNet.DocsTools.OspoClientServices;
+
+var client = await OspoClientFactory.CreateAsync();
+
+var link = await client.GetAsync("IEvangelist");
+
+if (link is not null)
+{
+    _ = link;
+}
