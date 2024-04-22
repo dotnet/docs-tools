ae617619324ffe1bc8f9ec3895b46f3179e45388
David Pine <david.pine@microsoft.com>
2024-04-22 14:24:17 -0500

    Minor clean up and switch to Bearer token header (#330)
    * Minor clean up and switch to Bearer token header
    
    * Add a test sandbox, and expose a means for getting a hydrated `OspoClient` via a factory that does `az login` and `az account get-access-token`.
    
    * A tweak or two

---
+++ DotNet.DocsTools/OspoClientServices/OspoClientFactory.cs
@@ -0,0 +1,69 @@
+﻿using CliWrap;
+using CliWrap.Buffered;
+using DotNetDocs.Tools.Utility;
+using Microsoft.DotnetOrg.Ospo;
+
+namespace DotNet.DocsTools.OspoClientServices;
+
+public static class OspoClientFactory
+{
+    public static async Task<OspoClient> CreateAsync()
+    {
+        await LoginAsync();
+
+        var token = await GetTokenAsync();
+
+        return new OspoClient(token, true);
+    }
+
+    private static async ValueTask LoginAsync()
+    {
+        // az login
+        var result = await Cli.Wrap("az")
+            .WithArguments(
+            [
+                "login",
+                "--scope",
+                "links"
+            ])
+            .WithValidation(CommandResultValidation.None)
+            .ExecuteAsync();
+
+        if (result.IsSuccess == false)
+        {
+            Console.WriteLine($"The 'az login' command supposedly failed with {result.ExitCode} after {result.RunTime}.");
+        }
+    }
+
+    private static async ValueTask<string> GetTokenAsync()
+    {
+        //var resource = CommandLineUtility.GetEnvVariable(
+        //    "OSMP_API_AUDIENCE", "Unable to get the scoped/resource.", null);
+
+        // az account get-access-token
+        //   --query 'accessToken'
+        //   -o tsv
+        //   --scope "links"
+        //   --resource "Some test audience"
+        var tokenResult = await Cli.Wrap("az")
+            .WithArguments(
+            [
+                "account",
+                "get-access-token",
+                "--query", "accessToken",
+                "-o", "tsv",
+                "--scope", "links",
+                //"--resource", resource
+            ])
+            .ExecuteBufferedAsync();
+
+        var token = tokenResult.StandardOutput.Trim();
+
+        if (string.IsNullOrEmpty(token))
+        {
+            Console.WriteLine($"az account get-access-token failed");
+        }
+
+        return token;
+    }
+}
