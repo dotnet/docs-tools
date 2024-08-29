f314d8b22d3ec5dc213060f9cbd51a908f226ce5
Genevieve Warren <24882762+gewarren@users.noreply.github.com>
2024-08-29 13:15:48 -0700

    [PackageIndexer] Include XML docs files for new assemblies (#393)

---
+++ PackageIndexer/FindProjectsWithDocs.ps1
@@ -0,0 +1,8 @@
+﻿Get-ChildItem -Path "C:\path\to\runtime\src\libraries" -Recurse -Filter "*.csproj" -File | 
+    Where-Object { $_.FullName -notlike "*\ref\*" -and $_.FullName -notlike "*\tests\*" -and $_.FullName -notlike "*\gen\*" -and $_.FullName -notlike "*\shims\*" -and $_.FullName -notlike "*\tools\*" -and $_.FullName -notlike "*\System.Private*\*" -and $_.FullName -notlike "*\Fuzzing\*" -and $_.FullName -notlike "*\externals.csproj" -and $_.FullName -notlike "*\Microsoft.NETCore.Platforms\*" -and $_.BaseName -notlike "System.Threading.RateLimiting" -and $_.BaseName -notlike "Microsoft.XmlSerializer.Generator" } | 
+    ForEach-Object {
+        $content = Get-Content -Path $_.FullName -Raw
+        if ($content -notmatch "UseCompilerGeneratedDocXmlFile") {
+            $_.BaseName
+        }
+    }
