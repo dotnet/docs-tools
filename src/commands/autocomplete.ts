ce2f0381f1894c125e7dae22c0729b6e28deb7fc
Andy De George (from Dev Box) <adegeo@microsoft.com>
2024-09-18 14:30:01 -0700

    Autocomplete has name with type preview!

--- src/commands/autocomplete.ts
+++ src/commands/autocomplete.ts
@@ -1,5 +1,5 @@
-import { CancellationToken, Position, ProviderResult, TextDocument, CompletionItemProvider, CompletionContext, CompletionItem, CompletionList } from "vscode";
-import { insertXrefLinkCommandName, urlFormatQuickPickItems } from '../consts';
+import { CancellationToken, Position, ProviderResult, TextDocument, CompletionItemProvider, CompletionContext, CompletionItem, CompletionList, CompletionItemKind } from "vscode";
+import { insertXrefLinkCommandName } from '../consts';
 import { UrlFormat } from './types/UrlFormat';
 import { SearchOptions } from './types/SearchOptions';
 
@@ -42,22 +42,41 @@ export const xrefDisplayTypeAutoComplete: CompletionItemProvider = {
                                       context: CompletionContext)
                                       : ProviderResult<CompletionList<CompletionItem> | CompletionItem[]> {
         
-        const range = document.getWordRangeAtPosition(position, /<xref:[^\s]+/);
+        const range = document.getWordRangeAtPosition(position, /<xref:[^\s>]+>/);
 
         if (range) {
 
-            const text = document.getText(range);
+            // Get the full name sans trailing * for overloads
+            let fullName = document.getText(range).replace('%2A', '').replace('*', '');
+            
+            // Trim off the ending regex result of ?>
+            fullName = fullName.substring(6, fullName.length - 2);
+            
+            let nameWithType = fullName;
+            
+            // If the full name has method () trim it down to (...) for display
+            if (nameWithType.indexOf('(') !== -1) {
+                nameWithType = `${nameWithType.substring(0, nameWithType.indexOf('('))}(…)`;
+            }
+
+            // If the full name has . in it, trim it down to the last two parts for name with type display
+            if (nameWithType.indexOf('.') !== -1) {
+                const items = nameWithType.split('.');
+                nameWithType = `${items.at(-2)}.${items.at(-1)}`;
+            }
 
             return [
                 {
-                    label: `$(array) Full name`,
-                    insertText: "displayProperty=fullName",
-                    detail: text
+                    label: 'Full name',
+                    insertText: 'displayProperty=fullName',
+                    detail: fullName,
+                    kind: CompletionItemKind.Unit
                 },
                 {
-                    label: `$(bracket-dot) Type with name`,
-                    insertText: "displayProperty=nameWithType",
-                    detail: text
+                    label: 'Type with name',
+                    insertText: 'displayProperty=nameWithType',
+                    detail: nameWithType,
+                    kind: CompletionItemKind.Unit
                 },
             ];
         }
