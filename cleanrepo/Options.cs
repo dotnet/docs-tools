40428e9e790b46b8c7d11d47106cd74da8780450
Genevieve Warren <24882762+gewarren@users.noreply.github.com>
2024-11-01 09:20:35 -0700

    set a default value for Delete

--- cleanrepo/Options.cs
+++ cleanrepo/Options.cs
@@ -6,7 +6,7 @@ class Options
     public string? DocFxDirectory { get; set; }
     public string? TargetDirectory { get; set; }
     public string? UrlBasePath { get; set; }
-    public bool? Delete { get; set; }
+    public bool Delete { get; set; } = true;
     public bool XmlSource { get; set; } = false;
     public string? Function { get; set; }
     public string? OcrModelDirectory { get; set; }
