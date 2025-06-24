f25ba22847a572625df83e886aaf0fafe512e025
Genevieve Warren <24882762+gewarren@users.noreply.github.com>
2025-06-24 18:41:11 +0200

    reset files

--- PackageIndexer/FindProjectsWithDocs.ps1
+++ PackageIndexer/FindProjectsWithDocs.ps1
@@ -1,4 +1,4 @@
-﻿Get-ChildItem -Path "C:\users\gewarren\runtime\src\libraries" -Recurse -Filter "*.csproj" -File | 
+﻿Get-ChildItem -Path "C:\path\to\runtime\src\libraries" -Recurse -Filter "*.csproj" -File | 
     Where-Object { $_.FullName -notlike "*\ref\*" -and $_.FullName -notlike "*\tests\*" -and $_.FullName -notlike "*\gen\*" -and $_.FullName -notlike "*\shims\*" -and $_.FullName -notlike "*\tools\*" -and $_.FullName -notlike "*\System.Private*\*" -and $_.FullName -notlike "*\Fuzzing\*" -and $_.FullName -notlike "*\externals.csproj" -and $_.FullName -notlike "*\Microsoft.NETCore.Platforms\*" -and $_.BaseName -notlike "System.Threading.RateLimiting" -and $_.BaseName -notlike "Microsoft.XmlSerializer.Generator" } | 
     ForEach-Object {
         $content = Get-Content -Path $_.FullName -Raw
