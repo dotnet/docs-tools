ee86485173b7397ac12db5cf6b42e7e127e403ad
Genevieve Warren <24882762+gewarren@users.noreply.github.com>
2026-05-13 09:19:50 -0700

    Update PackageIndexer/PlatformPackageDefinition.cs

--- PackageIndexer/PlatformPackageDefinition.cs
+++ PackageIndexer/PlatformPackageDefinition.cs
@@ -118,7 +118,7 @@ internal static class PlatformPackageDefinition
             PackageFilterExpression.Parse("Microsoft.Extensions.ML"),
             // Documented under .NET Aspire moniker.
             PackageFilterExpression.Parse("Microsoft.Extensions.ServiceDiscovery*"),
-            // Caused failure on 5/12/26 - see https://apidrop.visualstudio.com/6fb8cb7d-5623-420c-946b-ca74e63ac8ba/_apis/build/builds/607248/logs/34
+            // Fails to install in the CI pipeline - tracked by https://dev.azure.com/ceapex/Engineering/_workitems/edit/1146088.
             PackageFilterExpression.Parse("Microsoft.Extensions.Diagnostics.HealthChecks.EntityFrameworkCore"),
             // Public pkg but purely meant as a provider-facing helper.
             PackageFilterExpression.Parse("Microsoft.Extensions.VectorData.ConformanceTests"),
